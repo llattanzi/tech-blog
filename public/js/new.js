fbe4dc34293613650457bebf399fcba39afdf952
llattanzi <lauren71442@gmail.com>
2022-05-20 23:53:34 -0400

    add new post handlebars

--- public/js/new.js
+++ public/js/new.js
@@ -2,7 +2,7 @@ const newFormHandler = async function(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const body = document.querySelector('textarea[name="post-body"]').value;
+  const content = document.querySelector('textarea[name="post-body"]').value;
 
   await fetch(`/api/post`, {
     method: 'POST',
